9b0504ae9e2de0738a61536dd9a6dbd9ece47330
Amarachukwu Aguolu <amarachukwuaguolu@gmail.com>
2025-02-04 13:42:27 +0100

    I added templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,13 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=False)
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
     profile_picture = forms.ImageField(required=False)
-    bio = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 3}), 
-        required=False
-    )
+    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}),required=False)
     role = forms.ChoiceField(choices=Profile.ROLE_CHOICES)
     department = forms.CharField(max_length=100, required=False)
 
@@ -17,26 +16,21 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = [
             'username', 
+            'first_name',
+            'last_name',
             'email', 
             'password1', 
             'password2', 
-            'profile_picture', 
-            'bio', 
-            'role', 
-            'department'
         ]
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        
-        if commit:
-            user.save()
-            profile = user.profile
-            profile.profile_picture = self.cleaned_data.get('profile_picture')
-            profile.bio = self.cleaned_data.get('bio')
-            profile.role = self.cleaned_data.get('role')
-            profile.department = self.cleaned_data.get('department')
-            profile.save()
-        
-        return user
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'department']
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST, request.FILES)
+        form = UserRegisterForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
+            if 'profile_picture' in request.FILES:
+                user.profile.profile_picture = request.FILES['profile_picture']
+                user.profile.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Account created for {username}! You can Login')
             login(request, user)
             return redirect('todo_list')  # Redirect to todo list after registration
     else:
-        form = UserRegistrationForm()
+        form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
 
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html', {'user': request.user})
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, instance=request.user.profile)
+
+    context= {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'accounts/profile.html', context)
